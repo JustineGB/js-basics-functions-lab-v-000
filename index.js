2097c48387cdbdfa57363e27936b0d8b49f78236
Justine B <justinegayton@gmail.com>
2019-02-27 18:38:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function calculatesFarePrice(start, end) {
     return 0;}
   else if (400 >= x <= 2000) {
     return (x - 400) * 0.02;}
-  else if (2000 < x < 2500) {
+  else if (x > 2000 && x < 2500) {
     return 25;}
   else if (x >= 2500) {
     return 'cannot travel that far';}
