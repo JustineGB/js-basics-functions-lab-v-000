9f0c4bbe7f7247dff14f3af2260d8f37f72e6696
Justine B <justinegayton@gmail.com>
2019-02-27 17:29:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@
 function distanceFromHqInBlocks() {
   //returns a distance in blocks
 }
+
+
+//Finally, our last statement, expect(distanceFromHqInBlocks(43)).to.equal(1), tells us what our test is expecting the result to be. There are a couple really important clues here. Our first one is the 43. That tells us that our function is expecting an argument to be passed in, so we'll need to specify some kind of variable. Our other clue is the 1, because that tells us what the result is. If we're passing in 43, we're expecting our function to calculate that it's 1 block from headquarters. Try reading through all of the tests first, before you write any code! It will help you decide how to structure your code in the cleanest and most efficient manner.
