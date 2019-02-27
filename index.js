cf3a4441ad64e4dbfa48ae284eb018745dd6b389
Justine B <justinegayton@gmail.com>
2019-02-27 18:07:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function distanceFromHqInBlocks(someValue) {
-  if (someValue > 42){
-    return someValue - 42;
-  } else {
-    return 42 - someValue;
-  }
+  if (someValue > 42)
+    {return someValue - 42;} 
+  else 
+    {return 42 - someValue;}
 }
 
 function distanceFromHqInFeet(someValue) {
