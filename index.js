d7fb64abfd9683e70ed3936b03cab76661e41444
Justine B <justinegayton@gmail.com>
2019-02-27 17:49:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function distanceFromHqInBlocks(someValue) {
   }
 }
 
-// function distanceFromHqInFeet(someValue) {
+function distanceFromHqInFeet(someValue) {
   
-//   // distanceFromHqInBlocks(someValue);
-//   //returns a distance in feet
-// }
+  // distanceFromHqInBlocks(someValue);
+  //returns a distance in feet
+}
 
 // function distanceTravelledInFeet(someValue) {
 //   // Calculates the number of feet a passenger travels given a starting block and an ending block - it only calculates distance North and South (uptown/downtown). It uses the knowledge that a block is 264 feet long.
