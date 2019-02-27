bf8a89732f4e0a5cf97a706a0d754305c7e87eb4
Justine B <justinegayton@gmail.com>
2019-02-27 17:43:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ function distanceFromHqInBlocks(someValue) {
 
 distanceFromHqInBlocks();
 
-function distanceFromHqInFeet(someValue) {
+// function distanceFromHqInFeet(someValue) {
   
-  // distanceFromHqInBlocks(someValue);
-  //returns a distance in feet
-}
+//   // distanceFromHqInBlocks(someValue);
+//   //returns a distance in feet
+// }
 
-function distanceTravelledInFeet(someValue) {
-  // Calculates the number of feet a passenger travels given a starting block and an ending block - it only calculates distance North and South (uptown/downtown). It uses the knowledge that a block is 264 feet long.
-}
+// function distanceTravelledInFeet(someValue) {
+//   // Calculates the number of feet a passenger travels given a starting block and an ending block - it only calculates distance North and South (uptown/downtown). It uses the knowledge that a block is 264 feet long.
+// }
 
-function calculatesFarePrice(someValue) {
-  distanceTravelledInFeet(someValue);
-  // Calculates the number of feet a passenger travels given a starting block and an ending block - it only calculates distance North and South (uptown/downtown). It uses the knowledge that a block is 264 feet long.
-}
+// function calculatesFarePrice(someValue) {
+//   distanceTravelledInFeet(someValue);
+//   // Calculates the number of feet a passenger travels given a starting block and an ending block - it only calculates distance North and South (uptown/downtown). It uses the knowledge that a block is 264 feet long.
+// }
 
 
