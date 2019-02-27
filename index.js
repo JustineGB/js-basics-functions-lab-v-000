132946612a2eb121d7299dad374f60789b22ed70
Justine B <justinegayton@gmail.com>
2019-02-27 17:32:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,4 +17,8 @@ function distanceTravelledInFeet(someValue) {
   // Calculates the number of feet a passenger travels given a starting block and an ending block - it only calculates distance North and South (uptown/downtown). It uses the knowledge that a block is 264 feet long.
 }
 
+function calculatesFarePrice {
+  // Calculates the number of feet a passenger travels given a starting block and an ending block - it only calculates distance North and South (uptown/downtown). It uses the knowledge that a block is 264 feet long.
+}
+
 
